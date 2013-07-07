3c44914cbb8df965387c6f39bedc4c3f162c6e47
Joshua Lynch <joshua.kevin.lynch@gmail.com>
2013-07-06 21:36:17 -0500

    changed SMO training test to use same data as evaluation svm_test.py

--- svm_test.cpp
+++ svm_test.cpp
@@ -9,6 +9,7 @@
 
 #include "mothur/mothurout.h"
 #include "mothur/inputdata.h"
+
 #include "mothur/svm.hpp"
 
 
@@ -79,37 +80,103 @@ TEST(SmoTrainer, MoreThanTwoLabels) {
 
 // test SmoTrainer on four data points
 //
-// 1 * *
-// 0 * *
+// 1 + *
+// 0 + *
 //   012
 TEST(SmoTrainer, Train) {
     LabelVector labelVector;
-    labelVector.push_back("label_0");
-    labelVector.push_back("label_2");
-    labelVector.push_back("label_0");
-    labelVector.push_back("label_2");
+    labelVector.push_back("blue");
+    labelVector.push_back("blue");
+    labelVector.push_back("blue");
+    labelVector.push_back("blue");
+    labelVector.push_back("green");
+    labelVector.push_back("green");
+    labelVector.push_back("green");
+    labelVector.push_back("green");
 
-    FeatureVector x_01(2);
-    x_01[0] = 0.0;
-    x_01[1] = 0.0;
-    FeatureVector x_02(2);
-    x_02[0] = 0.0;
-    x_02[1] = 1.0;
-    FeatureVector x_21(2);
-    x_21[0] = 2.0;
-    x_21[1] = 0.0;
-    FeatureVector x_22(2);
-    x_22[0] = 2.0;
-    x_22[1] = 1.0;
-    ObservationVector observationVector(4);
-    observationVector[0] = &x_01;
-    observationVector[1] = &x_21;
-    observationVector[2] = &x_02;
-    observationVector[3] = &x_22;
+    //
+    // blue data points
+    //
+    FeatureVector x_blue_0(2);
+    x_blue_0[0] = 1.0;
+    x_blue_0[1] = 3.0;
+    FeatureVector x_blue_1(2);
+    x_blue_1[0] = 2.0;
+    x_blue_1[1] = 5.0;
+    FeatureVector x_blue_2(2);
+    x_blue_2[0] = 3.0;
+    x_blue_2[1] = 8.0;
+    FeatureVector x_blue_3(2);
+    x_blue_3[0] = 6.0;
+    x_blue_3[1] = 4.0;
+
+    //
+    // green data points
+    //
+    FeatureVector x_green_0(2);
+    x_green_0[0] = 6.0;
+    x_green_0[1] = 7.0;
+    FeatureVector x_green_1(2);
+    x_green_1[0] = 7.0;
+    x_green_1[1] = 8.0;
+    FeatureVector x_green_2(2);
+    x_green_2[0] = 8.0;
+    x_green_2[1] = 4.0;
+    FeatureVector x_green_3(2);
+    x_green_3[0] = 3.0;
+    x_green_3[1] = 6.0;
+    ObservationVector observationVector;
+    observationVector.push_back(&x_blue_0);
+    observationVector.push_back(&x_blue_1);
+    observationVector.push_back(&x_blue_2);
+    observationVector.push_back(&x_blue_3);
+    observationVector.push_back(&x_green_0);
+    observationVector.push_back(&x_green_1);
+    observationVector.push_back(&x_green_2);
+    observationVector.push_back(&x_green_3);
+    std::cout << "observation count: " << observationVector.size() << std::endl;
+    std::cout << "feature count: " << observationVector[0]->size() << std::endl;
+
+    // online method for mean and variance
+    for ( FeatureVector::size_type feature = 0; feature < observationVector[0]->size(); feature++ ) {
+        double n = 0.0;
+        double mean = 0.0;
+        double M2 = 0.0;
+        for ( ObservationVector::size_type observation = 0; observation < observationVector.size(); observation++ ) {
+            n += 1.0;
+            double x = observationVector[observation]->at(feature);
+            double delta = x - mean;
+            mean += delta / n;
+            M2 += delta * (x - mean);
+        }
+        double variance = M2 / (n - 1.0);
+        double standardDeviation = sqrt(variance);
+        std::cout << "mean of feature " << feature << " is " << mean << std::endl;
+        std::cout << "std of feature " << feature << " is " << standardDeviation << std::endl;
+        // normalize the feature
+        for ( ObservationVector::size_type observation = 0; observation < observationVector.size(); observation++ ) {
+            observationVector[observation]->at(feature) = (observationVector[observation]->at(feature) - mean ) / standardDeviation;
+        }
+    }
+
+    for ( ObservationVector::size_type i = 0; i < observationVector.size(); i++ ) {
+        std::cout << "i = " << i;
+        for ( FeatureVector::size_type j = 0; j < observationVector[0]->size(); j++ ) {
+            std::cout << " " << observationVector[i]->at(j);
+        }
+        std::cout << std::endl;
+    }
+
 
     SmoTrainer t;
     SVM* svm = t.train(observationVector, labelVector);
-    delete svm;
+
+    //EXPECT_EQ(-1, svm->discriminant(x_01));
+    //EXPECT_EQ(1, svm->discriminant(x_02));
+    //EXPECT_EQ(1, svm->discriminant(x_21));
+    //EXPECT_EQ(1, svm->discriminant(x_21));
+
+    //delete svm;
 }
 
 
